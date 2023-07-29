abfce5b493de017b97fcfd08c00930a46d71959b
Nitesh <niteshnagar1142002@gmail.com>
2023-07-29 17:48:44 +0530

    added mongodb databse

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,7 @@
 var mongoose = require('mongoose');
 var plm = require('passport-local-mongoose');
-const username = 'niteshnagar1142002';
-const password = encodeURIComponent('@12#Himani@12#'); // Encode the password
-const cluster = 'cluster0.xzjogp2.mongodb.net';
-const db = 'funstagram';
-// mongoose.connect('mongodb://127.0.0.1:27017/funstagram');
 
-let uri = `mongodb+srv://${username}:${password}@${cluster}/${db}?retryWrites=true&w=majority`;
+mongoose.connect('mongodb://127.0.0.1:27017/funstagram');
 
 // Connect to MongoDB using the constructed URI
 mongoose.connect(uri, {
